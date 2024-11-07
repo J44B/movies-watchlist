8fef36b9352ef45f925a0f84d24ee6b2bc69b589
jochen <jochen@byte23.net>
2024-11-07 10:31:40 +0100

    removed modal completely because of problems understanding it

--- src/components/components-index.js
+++ src/components/components-index.js
@@ -4,12 +4,13 @@ import { Header } from './Header';
 import { Footer } from './Footer';
 import { Movie } from './Movie';
 import { Button } from './Button';
-import { Modal } from './Modal';
+
+import { ModalButton } from './ModalButton';
 
 // -------------------------------------------- End import section  ----------------------------------------------
 
 // -------------------------------------------- Begin export section  --------------------------------------------
 
-export { Header, Footer, Movie, Button, Modal };
+export { Header, Footer, Movie, Button, ModalButton };
 
 // -------------------------------------------- End export section  --------------------------------------------
